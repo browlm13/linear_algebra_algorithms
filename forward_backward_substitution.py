f6c4524ee0b2afd4a75f6166476ee3d456573184
LJ Brown <browlm13@gmail.com>
2019-02-11 17:27:56 -0600

    Create forward_backward_substitution.py

---
+++ forward_backward_substitution.py
@@ -0,0 +1,118 @@
+
+"""
+	Forward / Backward Substitution - row / column oriented
+	Gausian Elimination
+
+
+"""
+
+import numpy as np
+
+#
+# Lx = b
+#
+
+def row_oriented_forward_substitution(L,b):
+
+	for i in range(L.shape[0]): 
+		for j in range(i):
+			b[i] -= L[i,j]*b[j]
+		b[i] = b[i]/L[i,i]
+	return b
+
+	"""
+	x = np.zeros(shape=b.shape)
+
+	for i in range(L.shape[0]): 
+		x[i] = b[i]
+		for j in range(i):
+			x[i] -= L[i,j]*x[j]
+		x[i] = x[i]/L[i,i]
+
+	return x
+	"""
+
+
+
+def column_oriented_forward_substitution(L,b):
+
+	for j in range(L.shape[1]): 
+		b[j] = b[j]/L[j,j]
+		for i in range(j+1,L.shape[0]):
+			b[i] -= L[i,j]*b[j]
+
+	return b
+
+#
+# Ux = y
+#
+
+def row_oriented_backward_substitution(U,y):
+
+	for i in range(U.shape[0]-1,-1,-1): 
+		for j in range(i+1, U.shape[1]):
+			y[i] -= U[i,j]*y[j]
+		y[i] = y[i]/U[i,i]
+		
+	return y
+
+	"""
+	x = np.zeros(shape=y.shape)
+
+	for i in range(U.shape[0]-1,-1,-1): 
+		x[i] = y[i]
+		for j in range(i+1, U.shape[1]):
+			x[i] -= U[i,j]*x[j]
+
+		x[i] = x[i]/U[i,i]
+		
+	return x
+	"""
+
+def column_oriented_backward_substitution(U,y):
+	for j in range(U.shape[1]-1,-1,-1):
+		y[j] = y[j]/U[j,j]
+		for i in range(j):
+			y[i] -= U[i,j]*y[j]
+		
+		
+	return y
+
+#
+# Testing
+#
+
+if __name__ == "__main__":
+
+	# create random matrix
+	n = 5
+	R = np.random.rand(n,n)*np.random.randint(low=1, high=100) # random matrix
+
+	#print(R)
+
+	# Get upper part for upper and lower triangular matrices
+	U = np.triu(R, 0) 
+	L = np.tril(R, 0)
+
+
+	# create random x
+	x_true = np.random.rand(n)*np.random.randint(low=1, high=100)
+
+	# find b and y 
+	b1 = L @ x_true
+	b2 = L @ x_true
+	y1 = U @ x_true
+	y2 = U @ x_true
+
+	# test substitution methods
+	x_rf = row_oriented_forward_substitution(L,b1)
+	x_cf = column_oriented_forward_substitution(L,b2)
+	x_rb = row_oriented_backward_substitution(U,y1)
+	x_cb = column_oriented_backward_substitution(U,y2)
+
+	# display errors
+	print(np.average(abs(x_true - x_rf)))
+	print(np.average(abs(x_true - x_cf)))
+	print(np.average(abs(x_true - x_rb)))
+	print(np.average(abs(x_true - x_cb)))
+
